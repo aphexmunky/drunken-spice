b153ab55eec7ec8533ff92ce0f602233424bc337
Josh Hesketh <josh.hesketh@gmail.com>
2015-06-30 00:34:20 +0100

    remove everything and start again

--- 03-Implementation/app/src/app_sup.erl
+++ 03-Implementation/app/src/app_sup.erl
@@ -8,5 +8,6 @@ start_link() ->
 	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-	Procs = [],
+	io:format("Hello, world!~n"),
+	Procs = [{'ANewName', {test_worker, start_link, []}, permanent, 1000, worker, [test_worker]}],
 	{ok, {{one_for_one, 1, 5}, Procs}}.
