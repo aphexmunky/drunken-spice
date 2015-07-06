c0be60f7bd40d3761845855932ee06fc73cd6363
Josh Hesketh <josh.hesketh@gmail.com>
2015-07-06 09:11:02 +0100

    created tcp supervisor but doesnt support multiple connections yet

--- 03-Implementation/app/src/app_sup.erl
+++ 03-Implementation/app/src/app_sup.erl
@@ -8,11 +8,9 @@ start_link() ->
 	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-	io:format("starting tcp connection...~n"),
-    {ok, LSock} = gen_tcp:listen(8888, [binary, {active, false}]),
-	io:format("started tcp connection...~n"),
+  	{ok, LSock} = gen_tcp:listen(8888, [binary, {active, true}]),
 	Procs = [
 		{udp_server, {udp, start_link, []}, permanent, 1000, worker, [udp]},
-		{tcp_server, {tcp, start_link, [LSock]}, permanent, 1000, worker, [tcp]}
+		{tcp_server, {tcp, start_link, [LSock]}, temporary, 1000, worker, [tcp]}
 	],
 	{ok, {{one_for_one, 1, 5}, Procs}}.
--- 03-Implementation/app/src/tcp.erl
+++ 03-Implementation/app/src/tcp.erl
@@ -16,23 +16,32 @@
 
 -record(state, {lsock}).
 
-start_link(LSock) ->
-  io:format("in tcp child~n"),
-  {ok, _Socket} = gen_tcp:accept(LSock),
-  gen_server:start_link({local, ?SERVER}, ?MODULE, [LSock], []).
+start_link(Args) ->
+  io:format("in tcp child with args: ~p~n", [Args]),
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [Args], []).
 
 code_change(_OldVsn, State, _Extra) ->
   io:format("tcp listener code_change~n"),
   {ok, State}.
 
 terminate(_Reason, _State) ->
-  io:format("tcp listener terminate~n"),
+  io:format("tcp listener terminate with ~p and ~p~n", [_Reason, _State]),
   ok.
 
+handle_info(trigger, State) ->
+  io:format("oh... ~p~n", [State]),
+  {ok, Socket} = gen_tcp:accept(State#state.lsock),
+  inet:setopts(State#state.lsock,[{active,once}]),
+  {noreply, #state{lsock = Socket}};
+handle_info({_,Socket,Msg}, State) ->
+  io:format("tcp listener handle_info with ~p and ~p~n", [[Socket, Msg], State]),
+  inet:setopts(State#state.lsock,[{active,once}]),
+  {noreply, State};
 handle_info(_Info, State) ->
-  io:format("tcp listener handle_info with ~p and ~p~n", [_Info, State]),
-  % {_Module, Socket, Address, Port, Msg} = _Info,
-  % handleTCP_transmission(Socket, Address, Port, Msg),
+  io:format("unknown message with ~p and ~p~n", [_Info, State]),
+  {_,Socket} = _Info,
+  gen_tcp:close(Socket),
+  inet:setopts(State#state.lsock,[{active,once}]),
   {noreply, State}.
 
 handle_cast(_Request, State) ->
@@ -43,10 +52,10 @@ handle_call(_Request, _From, State) ->
   io:format("tcp listener handle_call~n"),
   {reply, ok, State}.
 
-init(LSock) ->
-  io:format("tcp child init - opening socket ~p~n", [LSock]),
+init([_Something]) ->
+  io:format("tcp child init - opening socket ~p~n", [_Something]),
   erlang:send_after(0, self(), trigger),
-  {ok, #state{lsock = LSock}}.
+  {ok, #state{lsock = _Something}}.
 
 handleTCP_transmission(Socket, Packet) ->
   Res = storage:store(Packet),
